118e0377802bfdcf159dab39d85a8860f816b7c9
var_1 <jukainoito2016@gmail.com>
2020-07-10 13:12:49 +0800

    fix while time

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ async def main():
     await receiver.set_chat(args.chat)
     database = DynamoDB()
     count = 0
-    while count < 54:
+    while count < 55:
         msg = await receiver.receive_top_msg(args.sms_sign)
         database.put(msg)
         time.sleep(5)
